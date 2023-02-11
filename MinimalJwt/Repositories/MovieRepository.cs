67d28f858e0a735ba0d287dbd8a5ba719617107a
AzozoCode <josephbarrigah@gmail.com>
2023-02-11 00:51:13 +0000

    Add project files.

---
+++ MinimalJwt/Repositories/MovieRepository.cs
@@ -0,0 +1,24 @@
+﻿using MinimalJwt.Models;
+
+namespace MinimalJwt.Repositories
+{
+    public class MovieRepository
+    {
+        public static List<Movie> Movies = new() {
+            new(){ Id= 1,Title="Ghost",Description="Feature adaptation of Frank",Rating=6.8},
+
+            new(){Id= 2,Title="SpiderMan",Description="Feature Peter Parker",Rating=8.9},
+
+            new(){Id= 3,Title="AquaMan",Description="Feature Bruce Monn",Rating=8.9},
+
+            new(){Id= 4,Title="BatMan",Description="Feature Johnny Walker",Rating=8.9},
+
+            new(){Id= 5,Title="SuperMan",Description="Feature Batemio",Rating=8.9},
+
+            new(){Id= 6,Title="AntMan",Description="Feature Michael John",Rating=8.9},
+
+
+
+        };
+    }
+}
---
+++ MinimalJwt/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using MinimalJwt.Models;
+
+namespace MinimalJwt.Repositories
+{
+    public class UserRepository
+    {
+        public static List<User> Users = new()
+        {
+            new() { Username = "Luke_admin",EmailAddress="luke.admin@gmail.com",Password="MyPass_w0rd",GivenName="Luke",Surname="Roger",Role="Administrator"},
+
+            new() { Username = "randy_standard", EmailAddress = "randy.standard@gmail.com", Password = "MyPass_w0rd", GivenName = "Randy", Surname = "Joseph", Role = "Standard" }
+        };
+    }
+}
