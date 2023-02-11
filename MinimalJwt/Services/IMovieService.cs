e9eb277d6a1fc679f0b10727f213b1be6797ba73
AzozoCode <josephbarrigah@gmail.com>
2023-02-11 00:57:09 +0000

    initial commit

---
+++ MinimalJwt/Services/IMovieService.cs
@@ -0,0 +1,17 @@
+﻿using MinimalJwt.Models;
+
+namespace MinimalJwt.Services
+{
+    public interface IMovieService
+    {
+        public Movie GetMovie(int id);
+
+        public List<Movie> GetAllMovies();
+
+        public Movie PostMovie(Movie movie);
+
+        public bool DeleteMovie(int id);  
+        
+        public Movie PutMovie(Movie movie);
+    }
+}
